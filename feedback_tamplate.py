f29c38f194aa118a391d8209117ee474fea0de65
Rafal Jakobsen <rafal.jakobsen@protonmail.com>
2018-09-27 01:09:45 +0200

    Finishing the GUI with style

--- feedback_tamplate.py
+++ feedback_tamplate.py
@@ -6,6 +6,21 @@ class Feedback:
 
     def __init__(self, master):
 
+        #21. Program title
+        master.title("Explore California Feedback")
+        #22. Resize
+        master.resizable(False, False)
+        #23. Background
+        master.configure(background = "#e1d8b9")
+
+        #24. defult background for widget
+        self.style = ttk.Style()
+        self.style.configure("TFrame", background = "#e1d8b9")
+        self.style.configure("TButton", background="#e1d8b9")
+        self.style.configure("TLabel", background="#e1d8b9", font = ("Arial", 11))
+        self.style.configure("Header.TLabel", font = ("Arial", 18, "bold"))
+
+
         #1. Frame: header
         self.frame_header = ttk.Frame(master)
         #9. place into window with pack
@@ -14,9 +29,9 @@ class Feedback:
         #2. Label: logo(image)
         self.logo = PhotoImage(file = "tour_logo.gif")
 
-        #3. Label: header #11. add grid directly to code #15. added padding
+        #3. Label: header #11. add grid directly to code #15. added padding # 25. added style
         ttk.Label(self.frame_header, image=self.logo).grid(row=0, column=0, rowspan=2, padx = 5)
-        ttk.Label(self.frame_header, text='Thanks for Exploring!').grid(row=0, column=1, padx = 5)
+        ttk.Label(self.frame_header, text='Thanks for Exploring!', style = "Header.TLabel").grid(row=0, column=1, padx = 5)
         ttk.Label(self.frame_header, wraplength = 300,
                   text = "We`re glad you chose Explore California for your recent adventure.   " 
                   "Please tell us what you thought about the `Desert to Sea` tour").grid(row = 1, column = 1, padx = 5)
@@ -32,11 +47,11 @@ class Feedback:
         ttk.Label(self.frame_content, text="Comments: ").grid(row = 2, column = 0, padx = 5, sticky = "sw")
 
         #6. Entry: name, email
-        self.entry_name = ttk.Entry(self.frame_content, width = 24)
-        self.entry_email = ttk.Entry(self.frame_content, width = 24)
+        self.entry_name = ttk.Entry(self.frame_content, width = 24, font = ("Arial", 10))
+        self.entry_email = ttk.Entry(self.frame_content, width = 24, font = ("Arial", 10))
 
         #7. Text: comments
-        self.text_comments = Text(self.frame_content, width = 50, height = 10)
+        self.text_comments = Text(self.frame_content, width = 50, height = 10, font = ("Arial", 10))
 
         #13. call grid on entry variables
         self.entry_name.grid(row = 1, column = 0)
